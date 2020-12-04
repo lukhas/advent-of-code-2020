8dfc43683cf6d895ed8a8f63cbd810e3308b3a95
Lucas Bonnet <lbonnet@bearstech.com>
2020-12-04 16:30:01 +0100

    day1 solution: cheat and use a vector combination crate

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,21 +1,25 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+use combination::combine::*;
+
 fn main() -> std::io::Result<()> {
     // file must be in the directory from where we call the executable
     let mut file = File::open("day1_input")?;
     
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+    contents = contents.trim().to_string();
 
-    if contents.ends_with('\n') {
-        contents.pop();
-    }
-    
-    let l = contents.split('\n');
+    // make a u32 Vector out of the iter of strings
+    let l1: Vec<u32> = contents.split('\n').map(|x| x.parse().unwrap()).collect();
 
-    for val in l {
-        println!("Got: {}", val);
+    let val = combine_vec(&l1,2);
+    for tuple in val {
+        if tuple[0] + tuple[1] == 2020 {
+            println!("Matching tuple: {:?}", tuple);
+            println!("Solution: {}", tuple[0] * tuple[1]);
+        }
     }
 
     Ok(())
