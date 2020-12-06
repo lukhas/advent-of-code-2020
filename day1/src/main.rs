7761444c4645f06b8d4f515c17f6b7cd25407439
Lucas Bonnet <lbonnet@bearstech.com>
2020-12-06 17:18:00 +0100

    display result at the end, no one cares if you ran too many loops

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -12,22 +12,24 @@ fn main() -> std::io::Result<()> {
 
     // make a u32 Vector out of the iter of strings
     let number_list: Vec<u32> = contents.split('\n').map(|x| x.parse().expect("Input error")).collect();
-
+    let mut res_part1: u32 = 0;
+    let mut res_part2: u32 = 0;
+    
     for a in &number_list {
         for b in &number_list {
-            //println!("a,b: {} - {}",a, b);
             if a + b == 2020 {
-                println!("Matching tuple pt. 1: {} {}", a, b);
-                println!("Solution pt. 1: {}", a * b);
+                res_part1 = a * b;
             }
             for c in &number_list {
                 if a + b + c == 2020 {
-                    println!("Matching tuple pt. 2: {} {} {}", a, b, c);
-                    println!("Solution pt. 2: {}", a * b * c);
+                    res_part2 = a * b * c;
                 }
             }
         }
     }
+
+    println!("Part 1: {}", res_part1);
+    println!("Part 2: {}", res_part2);
     
     Ok(())
 }
