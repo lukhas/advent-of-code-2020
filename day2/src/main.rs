299dd028947dee4f4e06808b15e45dd9cc32643c
Lucas Bonnet <lbonnet@bearstech.com>
2020-12-11 18:32:16 +0100

    Part2 policy checking

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -12,13 +12,15 @@ fn main() -> std::io::Result<()> {
 
     let re = Regex::new(r"(\d+)-(\d+) ([a-z]): (.*)").unwrap();
 
-    let valid_count = re.captures_iter(str_contents).filter(|c| is_policy(&c)).count();
+    let valid_count1 = re.captures_iter(str_contents).filter(|c| is_policy1(&c)).count();
+    let valid_count2 = re.captures_iter(str_contents).filter(|c| is_policy2(&c)).count();
 
-    println!("Valid passwords: {}", valid_count);
+    println!("Valid passwords Pt. 1: {}", valid_count1);
+    println!("Valid passwords Pt. 2: {}", valid_count2);
     Ok(())
 }
 
-fn is_policy(cap: &regex::Captures) -> bool {
+fn is_policy1(cap: &regex::Captures) -> bool {
     let lower:usize = cap.get(1).unwrap().as_str().parse().expect("Parse failure");
     let upper:usize = cap.get(2).unwrap().as_str().parse().expect("Parse failure");
     // it's actually just one character, trust me
@@ -29,4 +31,20 @@ fn is_policy(cap: &regex::Captures) -> bool {
     let letter_count:usize = pw.chars().filter(|c| c == &letter ).count();
     //println!("Letter {} found {} times in {}", letter, letter_count, pw);
     (lower <= letter_count) && (letter_count <= upper)
+}
+
+fn is_policy2(cap: &regex::Captures) -> bool {
+    let p1:usize = cap.get(1).unwrap().as_str().parse().expect("Parse failure");
+    let p2:usize = cap.get(2).unwrap().as_str().parse().expect("Parse failure");
+    // it's actually just one character, trust me
+    let letter:char = cap.get(3).unwrap().as_str().chars().nth(0).unwrap();
+    let pw:String = String::from(cap.get(4).unwrap().as_str());
+    //println!("Lower: {} Upper: {} Letter: {}", lower, upper, letter);
+
+    // safety first
+    if (pw.chars().count() < p1) || (pw.chars().count() < p2) {
+        return false;
+    }
+
+    (pw.chars().nth(p1-1).unwrap() == letter) ^ (pw.chars().nth(p2-1).unwrap() == letter)
 }
