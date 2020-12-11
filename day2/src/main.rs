4d27c1cfbf781e46ee606e0596a4e4bd3fb7b543
Lucas Bonnet <lbonnet@bearstech.com>
2020-12-11 17:37:12 +0100

    a tad more idiomatic day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,37 +8,25 @@ fn main() -> std::io::Result<()> {
     
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    contents = contents.trim().to_string();
+    let str_contents = contents.trim();
 
-    let mut valid_count: usize = 0;
+    let re = Regex::new(r"(\d+)-(\d+) ([a-z]): (.*)").unwrap();
 
-    // match
-    let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): (.*)$").unwrap();
-
-    for line in contents.split('\n') {
-        match re.captures(line) {
-            Some(cap) => valid_count = valid_count + is_policy(&cap),
-            None    => println!("Ignoring broken line: {}", line)
-        }
-    }
+    let valid_count = re.captures_iter(str_contents).filter(|c| is_policy(&c)).count();
 
     println!("Valid passwords: {}", valid_count);
     Ok(())
 }
 
-fn is_policy(cap: &regex::Captures) -> usize {
+fn is_policy(cap: &regex::Captures) -> bool {
     let lower:usize = cap.get(1).unwrap().as_str().parse().expect("Parse failure");
     let upper:usize = cap.get(2).unwrap().as_str().parse().expect("Parse failure");
     // it's actually just one character, trust me
     let letter:char = cap.get(3).unwrap().as_str().chars().nth(0).unwrap();
     let pw:String = String::from(cap.get(4).unwrap().as_str());
-    // println!("Lower: {} Upper: {} Letter: {}", lower, upper, letter);
+    //println!("Lower: {} Upper: {} Letter: {}", lower, upper, letter);
     
     let letter_count:usize = pw.chars().filter(|c| c == &letter ).count();
     //println!("Letter {} found {} times in {}", letter, letter_count, pw);
-    if (lower <= letter_count) && (letter_count <= upper) {
-        return 1;
-    } else {
-        return 0;
-    }
+    (lower <= letter_count) && (letter_count <= upper)
 }
