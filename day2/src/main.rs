34d47ea378dc887c14e41e46c8801eb93a3bd22b
Lucas Bonnet <lbonnet@bearstech.com>
2020-12-11 17:19:08 +0100

    day2 slightly more idiomatic

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,28 +10,35 @@ fn main() -> std::io::Result<()> {
     file.read_to_string(&mut contents)?;
     contents = contents.trim().to_string();
 
+    let mut valid_count: usize = 0;
+
     // match
     let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): (.*)$").unwrap();
 
-    let mut valid_count = 0;
-    
     for line in contents.split('\n') {
-        for cap in re.captures_iter(line) {
-            let lower:usize = cap[1].parse().expect("Parse failure");
-            let upper:usize = cap[2].parse().expect("Parse failure");
-            // it's actually just one character, trust me
-            let letter:char = cap[3].chars().nth(0).unwrap();
-            let pw:String = String::from(&cap[4]);
-            // println!("Lower: {} Upper: {} Letter: {}", lower, upper, letter);
-
-            let letter_count:usize = pw.chars().filter(|c| c == &letter ).count();
-            //println!("Letter {} found {} times in {}", letter, letter_count, pw);
-            if (lower <= letter_count) && (letter_count <= upper) {
-                valid_count = valid_count + 1;
-            }
+        match re.captures(line) {
+            Some(cap) => valid_count = valid_count + is_policy(&cap),
+            None    => println!("Ignoring broken line: {}", line)
         }
     }
 
     println!("Valid passwords: {}", valid_count);
     Ok(())
+}
+
+fn is_policy(cap: &regex::Captures) -> usize {
+    let lower:usize = cap.get(1).unwrap().as_str().parse().expect("Parse failure");
+    let upper:usize = cap.get(2).unwrap().as_str().parse().expect("Parse failure");
+    // it's actually just one character, trust me
+    let letter:char = cap.get(3).unwrap().as_str().chars().nth(0).unwrap();
+    let pw:String = String::from(cap.get(4).unwrap().as_str());
+    // println!("Lower: {} Upper: {} Letter: {}", lower, upper, letter);
+    
+    let letter_count:usize = pw.chars().filter(|c| c == &letter ).count();
+    //println!("Letter {} found {} times in {}", letter, letter_count, pw);
+    if (lower <= letter_count) && (letter_count <= upper) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
