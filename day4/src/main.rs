514d6dd50a51dde6e58ac39f9faff86c2a5607c2
Lukhas <lbonnet@bearstech.com>
2020-12-25 19:00:02 +0100

    day4, pt.2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,9 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+#[macro_use] extern crate lazy_static;
+use regex::Regex;
+
 fn main() -> std::io::Result<()> {
     // file must be in the directory from where we call the executable
     let mut file = File::open("day4_input")?;
@@ -14,18 +17,109 @@ fn main() -> std::io::Result<()> {
 }
 
 fn is_valid_passport(passport: &str) -> bool {
-    let fields = [("byr:", 0b0000001),
-                  ("iyr:", 0b0000010),
-                  ("eyr:", 0b0000100),
-                  ("hgt:", 0b0001000),
-                  ("hcl:", 0b0010000),
-                  ("ecl:", 0b0100000),
-                  ("pid:", 0b1000000)];
+    // println!("");
+    // println!("{}",passport);
 
-    fields.iter().fold(0b0, |tot, field| {
-        match passport.find(field.0) {
-            None    => tot,
-            Some(_) => tot + field.1,
+    has_valid_byr(passport) &&
+        has_valid_iyr(passport) &&
+        has_valid_eyr(passport) &&
+        has_valid_hgt(passport) &&
+        has_valid_hcl(passport) &&
+        has_valid_ecl(passport) &&
+        has_valid_pid(passport)
+}
+
+fn has_valid_byr(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"byr:([0-9]{4})").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(x) => {
+            let year = number_from_match(&x);
+            (year >= 1920) && (year <= 2002)
         }
-    }) == 0b1111111
+    }
+}
+
+fn has_valid_iyr(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"iyr:([0-9]{4})").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(x) => {
+            let year = number_from_match(&x);
+            (year >= 2010) && (year <= 2020)
+        }
+    }
+}
+
+fn has_valid_eyr(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"eyr:([0-9]{4})").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(x) => {
+            let year = number_from_match(&x);
+            (year >= 2020) && (year <= 2030)
+        }
+    }
+}
+
+fn has_valid_hgt(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"hgt:([0-9]+)(in|cm)").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(x) => {
+            let hgt =  number_from_match(&x);
+            let unit = x.get(2).unwrap().as_str();
+            (unit == "cm" && hgt >= 150 && hgt <= 193) ||
+                (unit == "in" && hgt >= 59 && hgt <= 76)
+        }
+    }
+}
+
+fn has_valid_hcl(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"hcl:#[0-9a-f]{6}").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(_) => true
+    }
+}
+
+fn has_valid_ecl(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(_) => true
+    }
+}
+
+fn has_valid_pid(passport: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"pid:([0-9]{9})").unwrap();
+    }
+
+    match RE.captures(passport) {
+        None    => false,
+        Some(_) => true
+    }
+}
+
+fn number_from_match(field: &regex::Captures) -> u32 {
+    field.get(1).unwrap().as_str().parse().unwrap()
 }
